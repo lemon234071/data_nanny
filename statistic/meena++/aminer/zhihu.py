a9e5246e46a29b6496099804a9c3f7bf5ce2a340
lemon234071 <79388548@qq.com>
2021-01-12 15:11:39 +0800

    Fix bug: zhihu.py cnt

--- statistic/meena++/aminer/zhihu.py
+++ statistic/meena++/aminer/zhihu.py
@@ -17,24 +17,34 @@ def sta_zhihu(rootdirpath):
     subsubsubdirs = [os.path.join(subsubdir, x) for subsubdir in subsubdirs for x in os.listdir(subsubdir) if
                      os.path.isdir(os.path.join(subsubdir, x))]
     cnt_fail = 0
-    cnt_dialog = 0
+    cnt_qa, cnt_ans, cnt_comment = 0, 0, 0
     for thisdir in tqdm.tqdm(subsubsubdirs):
         path = os.path.join(thisdir, "zhihu")
         if os.path.exists(path):
             with open(path, encoding="utf-8") as f:
                 data = [json.loads(x) for x in f.readlines()]
         elif os.path.exists(path + ".gz"):
-            with gzip.open(path, "rb", encoding="utf-8") as f:
+            with gzip.open(path + ".gz", "rb", encoding="utf-8") as f:
                 data = [json.loads(x) for x in f.readlines()]
         else:
             cnt_fail += 1
             print(thisdir)
             continue
-        print(data[0])
+        # print(data[0])
         print(thisdir)
-        cnt_dialog += len(data)
+        if "qa" in thisdir:
+            cnt_qa += len(data)
+        elif "answer" in thisdir:
+            cnt_ans += len(data)
+        elif "comments" in thisdir:
+            cnt_comment += len(data)
+        else:
+            cnt_fail += 1
     print(cnt_fail, "failed")
-    print("dialog:", cnt_dialog)
+    print("qa:", cnt_qa)
+    print("ans:", cnt_ans)
+    print("comments:", cnt_comment)
+    print("statistic zhihu over")
 
 
 if __name__ == '__main__':
